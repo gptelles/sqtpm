cd82bd7a523da9582ceed291e41a04849c04d33d
gptelles <gptelles@gmail.com>
2021-02-28 11:38:27 -0300

    9

---
+++ Utils/link-assigns.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Link .pass files into every subdirectory that has a config file.
+
+pwd
+
+pass=`ls -1 *.pass`
+
+for d in */; do 
+  cd "$d"
+
+  if [[ -e 'config' ]]; then
+    for l in $pass; do 
+      ln -s ../$l .
+    done 
+  fi
+
+  cd ..
+done
+
+
+
